34e7a11b18ee0cbba62da0237e1d663de7103fc7
Egon Willighagen <egon.willighagen@gmail.com>
2022-01-06 09:46:46 +0100

    Added the first (currently failing) test for nano InChIs

---
+++ src/test/java/org/bitbucket/nanojava/data/NanoInChIExamplesTest.java
@@ -0,0 +1,58 @@
+/* Copyright (C) 2022  Egon Willighagen <egonw@users.sf.net>
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package org.bitbucket.nanojava.data;
+
+import java.io.ByteArrayOutputStream;
+
+import org.bitbucket.nanojava.inchi.NInChIGenerator;
+import org.bitbucket.nanojava.io.CDKDeserializer;
+import org.bitbucket.nanojava.io.CDKSerializer;
+import org.junit.Assert;
+import org.junit.Test;
+import org.xmlcml.cml.element.CMLMoleculeList;
+
+import nu.xom.Document;
+import nu.xom.Serializer;
+
+public class NanoInChIExamplesTest {
+
+	@Test
+	public void figureEightLeft() throws Exception {
+		Material material = MaterialBuilder.type("METALOXIDE")
+			.label("silica nanoparticles with gold coating")
+			.componentFromSMILES(1, "O=[Si]=O")
+			.componentFromSMILES(2, "[Au]")
+			.asMaterial();
+
+		CMLMoleculeList cmlMaterial = CDKSerializer.toCML(material);
+		Assert.assertNotNull(cmlMaterial);
+		System.out.println(asIndentedString(cmlMaterial));
+
+		String nanoInChI = NInChIGenerator.generator(material);
+		Assert.assertNotNull(cmlMaterial);
+		Assert.assertEquals("InChI=1A/Au/msh/s2t-9!O2Si/c1-3-2/msp/s20d-9/k000/y2&1", nanoInChI);
+	}
+
+	private String asIndentedString(CMLMoleculeList cmlMaterial) throws Exception {
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		Serializer xomSerializer = new Serializer(output, "UTF-8");
+		xomSerializer.setIndent(2);
+		xomSerializer.write(new Document(cmlMaterial));
+		return output.toString();
+	}
+
+}
